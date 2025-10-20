bce23ef0a9609cee49b73be8c37d6182223a1dee
Bence Fekete <fekete965@gmail.com>
2025-10-20 11:17:10 +0100

    Add basic REPL functionality

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Pokedex > ")
+	
+	for scanner.Scan() {
+		cleanedInput := cleanInput(scanner.Text())
+		fmt.Println("Your command was: " + cleanedInput[0])
+		fmt.Print("Pokedex > ")
+	}
+}
+
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+
+	for i, word := range words {
+		words[i] = strings.ToLower(word)
+	}
+
+	return words
 }
---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+		input:    "  hello  world  ",
+		expected: []string{"hello", "world"},
+		},
+		{
+			input: "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		actualLength := len(actual)
+		expectedLength := len(c.expected)
+
+		if actualLength != expectedLength {
+			t.Errorf("expected length to be %v, got %v", expectedLength, actualLength)
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("expected %v got %v", expectedWord, word)
+			}
+		}
+	}
+}
