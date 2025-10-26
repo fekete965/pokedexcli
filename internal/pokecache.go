cf2ee47d0565273a2846b0aa6b0db1aaaaa945be
Bence Fekete <fekete965@gmail.com>
2025-10-26 08:30:19 +0000

    Add pokecache

---
+++ internal/pokecache.go
@@ -0,0 +1,68 @@
+package internal
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu sync.Mutex
+	entry map[string] cacheEntry
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	newCache := Cache{
+		mu: sync.Mutex{},
+		entry: make(map[string] cacheEntry),
+	}
+
+	go newCache.reapLoop(interval)
+
+	return &newCache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.entry[key] = cacheEntry{
+		createdAt: time.Now(),
+		val: val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	
+	if entry, hasEntry := c.entry[key]; hasEntry {
+		return entry.val, true
+	}
+
+	return nil, false
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		c.deleteExpiresEntries(interval)
+	}
+}
+
+func (c *Cache) deleteExpiresEntries(interval time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for key, entry := range c.entry {
+		if (time.Since(entry.createdAt) > interval) {
+			delete(c.entry, key)
+		}
+	}
+}
---
+++ internal/pokecache_test.go
@@ -0,0 +1,68 @@
+package internal
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestAddGet(t *testing.T) {
+	const interval = time.Second * 5
+
+	cases := []struct {
+		key string
+		val []byte
+	} {
+		{
+			key: "https://test-key.com",
+			val: []byte("test-data"),
+		},
+		{
+			key: "https://test-key.com/path",
+			val: []byte("additional-test-data"),
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Text case %v", i), func(t *testing.T) {
+			cache := NewCache(interval)
+			cache.Add(c.key, c.val)
+
+			val, ok := cache.Get(c.key)
+
+			if !ok {
+				t.Errorf("expected to find key: %v", c.key)
+				return
+			}
+
+			if string(val) != string(c.val) {
+				t.Errorf("expected to find the correct value")
+				return
+			}
+		})
+	}
+}
+
+func TestReapLoop(t *testing.T) {
+	const baseTime = time.Millisecond * 5
+	const waitTime = baseTime + time.Millisecond * 5
+
+	testCacheKey := "https://test-key.com"
+
+	cache := NewCache(baseTime)
+	cache.Add(testCacheKey, []byte("testdata"))
+
+	_, ok:= cache.Get(testCacheKey)
+	if !ok {
+		t.Errorf("expected to find key: %v", testCacheKey)
+		return
+	}
+
+	time.Sleep(waitTime)
+
+	_, ok = cache.Get(testCacheKey)
+	if ok {
+		t.Errorf("expected to not find key: %v", testCacheKey)
+		return 
+	}
+}
