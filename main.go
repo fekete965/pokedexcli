4155afa573c25babf47490c9eb2f909f3c3969d3
Bence Fekete <fekete965@gmail.com>
2025-10-27 20:42:22 +0000

    Rename PokemonLocationAreaItem to NamedResource

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 
 const LOCATION_API_URL string = "https://pokeapi.co/api/v2/location-area/"
 
-type PokemonLocationAreaItem struct {
+type NamedResource struct {
 	Name string `json:"name"`
 	Url string `json:"url"`
 }
@@ -25,7 +25,8 @@ type PokemonLocationAreaResponse struct {
 	Count int `json:"count"`
 	Next *string `json:"next"`
 	Previous *string `json:"previous"`
-	Results []PokemonLocationAreaItem `json:"results"`
+	Results []NamedResource `json:"results"`
+}
 }
 
 type cliCommand struct {
